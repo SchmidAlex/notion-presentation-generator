b83916e7699c9620607a8639e0e5f3df144de193
Alex <91736634+SchmidAlex@users.noreply.github.com>
2025-01-28 14:58:46 +0100

    add multi answer to the poll plugin

--- helpers/plugins/poll/plugin.js
+++ helpers/plugins/poll/plugin.js
@@ -2,9 +2,9 @@
 ** Author: Asvin Goel, goel@telematique.eu
 **
 ** A plugin for reveal.js adding instant polls within an 
-** online seminar.
+** online seminar. Updated for Multi-Select functionality.
 **
-** Version: 0.1.1
+** Version: 0.2.0
 **
 ** License: MIT license (see LICENSE.md)
 **
@@ -17,120 +17,160 @@ window.RevealPoll = window.RevealPoll || {
     },
 };
 
+const initPoll = function (Reveal) {
+    var config = Reveal.getConfig().poll;
 
-const initPoll = function(Reveal){
-	var config = Reveal.getConfig().poll;
+    var polls = [];
 
-	var polls = [];
+    // Get poll index
+    function getPollIndex(id) {
+        return polls.findIndex((poll) => poll.id === id);
+    }
 
-	// Get poll index
-	function getPollIndex(id) {
-		return polls.findIndex(poll => poll.id === id);
-	}
+    function initializePolls() {
+        var pollElements = document.querySelectorAll(".poll");
 
-	function initializePolls() {
-		var pollElements = document.querySelectorAll(".poll");
-//console.log(polls);
-		for (var i = 0; i < pollElements.length; i++ ){
-			var id = pollElements[i].getAttribute('data-poll')
-			var votes = {};
-			var buttons = pollElements[i].querySelectorAll("button");
-			for (var j = 0; j < buttons.length; j++ ){
-				// initialize number of votes for button
-				votes[buttons[j].getAttribute('data-value')] = 0; 
+        for (var i = 0; i < pollElements.length; i++) {
+            var id = pollElements[i].getAttribute("data-poll");
+            var multiSelect = pollElements[i].getAttribute("data-multi"); // New attribute for multi-select
+            var votes = {};
+            var buttons = pollElements[i].querySelectorAll("button");
 
-				// make button clickable
-				buttons[j].addEventListener('click', function(evt){
-					if ( !RevealSeminar.connected() ) {
-						alert("You are currently not connected to the live poll. Your vote is ignored.");
-						return;
-					}
-					const button = evt.target;
-					const poll = button.parentElement;
-					var siblings = poll.querySelectorAll("button");
-					for (var i = 0; i < siblings.length; i++ ){
-						siblings[i].disabled = true;
-					}
-					vote( poll.getAttribute('data-poll'), button.getAttribute('data-value') );
-					button.classList.add("selected"); 
-					button.blur();
-				});
-			}
-			polls.push( { id, voters: 0, votes} );
-//console.log(polls);
-		}
+            for (var j = 0; j < buttons.length; j++) {
+                // Initialize number of votes for button
+                votes[buttons[j].getAttribute("data-value")] = 0;
 
-	}
+                // Make button clickable
+                buttons[j].addEventListener("click", function (evt) {
+                    if (!RevealSeminar.connected()) {
+                        alert("You are currently not connected to the live poll. Your vote is ignored.");
+                        return;
+                    }
 
-	function vote( poll, choice ) {
-		// send to vote to chair
-		var message = new CustomEvent('send');
-		message.content = { sender: 'poll-plugin', recipient: true, type: 'vote', poll, choice };
-		document.dispatchEvent( message );
-	}
+                    const button = evt.target;
+                    const poll = button.parentElement;
 
-	document.addEventListener( 'received', function ( message ) {
-		if ( message.content && message.content.sender == 'poll-plugin' ) {
-//console.log("Update: ", message.content);
-			if ( message.content.type == 'vote' ) {
-				const vote = message.content;
-				const poll = polls[getPollIndex(message.content.poll)];
-				// increment number of voters
-				poll.voters++;
-				var message = new CustomEvent('broadcast');
-				message.content = { sender: 'poll-plugin', copy: true, type: 'voters', poll: poll.id, voters: poll.voters };
-				document.dispatchEvent( message );
+                    if (multiSelect) {
+                        // Toggle selection for multi-select
+                        button.classList.toggle("selected");
+                        button.blur();
+                    } else {
+                        // Single select: disable all buttons after vote
+                        var siblings = poll.querySelectorAll("button");
+                        for (var i = 0; i < siblings.length; i++) {
+                            siblings[i].disabled = true;
+                        }
+                        button.classList.add("selected");
+                        button.blur();
+                    }
 
-				// update results
-				poll.votes[vote.choice]++;
-//console.log("Vote '" + vote.choice + "' received for poll ", poll );
-				message = new CustomEvent('broadcast');
-				message.content = { sender: 'poll-plugin', copy: true, type: 'results', poll: poll.id, votes: poll.votes };
-//console.log("Send results", message );
-				document.dispatchEvent( message );
-			}
-			else if ( message.content.type == 'voters' ) {
-//console.log("voters", message.content )
-				var voters = document.querySelectorAll('.voters[data-poll="' + message.content.poll + '"]');
-				for (var j = 0; j < voters.length; j++ ){
-					voters[j].innerHTML = message.content.voters;
-				}
+                    // Collect all selected votes
+                    const selectedVotes = Array.from(
+                        poll.querySelectorAll("button.selected")
+                    ).map((btn) => btn.getAttribute("data-value"));
 
-			}
-			else if ( message.content.type == 'results' ) {
-				// update result elements
-				var results = document.querySelectorAll('.results[data-poll="' + message.content.poll + '"]');
-//console.log("Results", results )
-				for (var i = 0; i < results.length; i++ ) {
-//console.log("Votes", message.content.votes )
-					for (var choice in message.content.votes) {
-//console.log(choice);
-						var elements = results[i].querySelectorAll('[data-value="' + choice + '"]');
-						for (var j = 0; j < elements.length; j++ ) {
-							elements[j].innerHTML = message.content.votes[choice];
-						}
-					}
-				}
+                    // Submit votes for multi-select or single-select
+                    vote(poll.getAttribute("data-poll"), selectedVotes);
+                });
+            }
 
-				// update result charts
-				if ( RevealChart ) {
-					var charts = document.querySelectorAll('canvas[data-chart][data-poll="' + message.content.poll + '"]');	
-					var data = [];
-					for (var choice in message.content.votes) {
-						data.push(message.content.votes[choice]);
-					}
-					for (var i = 0; i < charts.length; i++ ) {
-						RevealChart.update( charts[i], 0, data );
-					}
-				}
-			}
-		}
-	});
+            polls.push({ id, multi: multiSelect, voters: 0, votes });
+        }
+    }
 
-	Reveal.addEventListener('ready', function(){
-//alert("READY");
-		initializePolls();
-	});
+    function vote(poll, choices) {
+        // Send vote(s) to chair
+        var message = new CustomEvent("send");
+        message.content = {
+            sender: "poll-plugin",
+            recipient: true,
+            type: "vote",
+            poll,
+            choices, // Changed to an array to support multiple votes
+        };
+        document.dispatchEvent(message);
+    }
 
-	return this;
+    document.addEventListener("received", function (message) {
+        if (message.content && message.content.sender == "poll-plugin") {
+            if (message.content.type == "vote") {
+                const vote = message.content;
+                const poll = polls[getPollIndex(message.content.poll)];
+
+                // Increment number of voters
+                poll.voters++;
+                var voterMessage = new CustomEvent("broadcast");
+                voterMessage.content = {
+                    sender: "poll-plugin",
+                    copy: true,
+                    type: "voters",
+                    poll: poll.id,
+                    voters: poll.voters,
+                };
+                document.dispatchEvent(voterMessage);
+
+                // Update results for each choice
+                vote.choices.forEach((choice) => {
+                    poll.votes[choice] = (poll.votes[choice] || 0) + 1;
+                });
+
+                // Broadcast updated results
+                var resultsMessage = new CustomEvent("broadcast");
+                resultsMessage.content = {
+                    sender: "poll-plugin",
+                    copy: true,
+                    type: "results",
+                    poll: poll.id,
+                    votes: poll.votes,
+                };
+                document.dispatchEvent(resultsMessage);
+            } else if (message.content.type == "voters") {
+                // Update voter count
+                var voters = document.querySelectorAll(
+                    '.voters[data-poll="' + message.content.poll + '"]'
+                );
+                for (var j = 0; j < voters.length; j++) {
+                    voters[j].innerHTML = message.content.voters;
+                }
+            } else if (message.content.type == "results") {
+                // Update results display
+                var results = document.querySelectorAll(
+                    '.results[data-poll="' + message.content.poll + '"]'
+                );
+
+                for (var i = 0; i < results.length; i++) {
+                    for (var choice in message.content.votes) {
+                        var elements = results[i].querySelectorAll(
+                            '[data-value="' + choice + '"]'
+                        );
+                        for (var j = 0; j < elements.length; j++) {
+                            elements[j].innerHTML = message.content.votes[choice];
+                        }
+                    }
+                }
+
+                // Update charts if RevealChart is used
+                if (RevealChart) {
+                    var charts = document.querySelectorAll(
+                        'canvas[data-chart][data-poll="' +
+                            message.content.poll +
+                            '"]'
+                    );
+                    var data = [];
+                    for (var choice in message.content.votes) {
+                        data.push(message.content.votes[choice]);
+                    }
+                    for (var i = 0; i < charts.length; i++) {
+                        RevealChart.update(charts[i], 0, data);
+                    }
+                }
+            }
+        }
+    });
+
+    Reveal.addEventListener("ready", function () {
+        initializePolls();
+    });
+
+    return this;
 };
